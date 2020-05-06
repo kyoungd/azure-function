71244ae8123c78df1cfe99cc7144ec9645676f64
young <kyoungd@hotmail.com>
2020-05-06 15:04:58 -0700

    added saveRawData function

--- SaveRawDataToDb/index.js
+++ SaveRawDataToDb/index.js
@@ -1,13 +1,24 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    if (req.method === "POST") {
+        try {
+            const dataBlock = req.body.value;
+            await commitRawData(dataBlock);
+            context.res = {
+                status: 200, /* Defaults to 200 */
+                body: "OK"
+            };
+        } catch (e) {
+            context.res = {
+            status: 500 /* Defaults to 200 */,
+            body: e.message
+            };
+        }    
+    }
+    else 
+        context.res = {
+            status: 200, /* Defaults to 200 */
+            body: "http post only"
+        };
 }
