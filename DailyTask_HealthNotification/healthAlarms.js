97e24ed4a7f34260b058ca71ec14eaab6b2c7dc2
young <kyoungd@hotmail.com>
2020-05-15 23:26:43 -0700

    updated healthAlarms

--- DailyTask_HealthNotification/healthAlarms.js
+++ DailyTask_HealthNotification/healthAlarms.js
@@ -1,17 +1,38 @@
-const _ = require("lodash");
+const emailMessagesByCode = require('./emailMessageByCode.json');
 
-function healthAlarms(contact, analysisObject) {
-  let message = "";
+const sendNotification = (contacts, smsMessage, emailmessage) => {
+  this.smsMessage = smsMessage;
+  this.emailmessage = emailmessage;
+  if (this.smsMessage) {
+    contacts.map(contact => {
+      if (contact.isNotify) {
+        console.log(contact.phone);
+        console.log(contact.email);
+        console.log(this.smsMessage);
+        console.log(this.emailmessage)
+      }
+    }, this);
+  }
+}
+
+function healthAlarms (contacts, analysisObject) {
+  let smsMessage = "";
+  let emailMessage = "";
   const mainKeys = Object.keys(analysisObject);
   for (let idx = 0; idx < mainKeys.length; ++idx) {
     const key = mainKeys[idx];
     const alertObject = analysisObject[key];
     for (let ix = 0; ix < alertObject.length; ++ix) {
       let obj = alertObject[ix];
-      if (obj.errorCount > obj.errorAllowed) {
-        smsMessage += key + "." + obj.key;
-        emailMessage += emailMessageByCode[key][obj.key];
+      const firstKey = Object.keys(obj)[0];
+      const analysis = obj[firstKey];
+      if (analysis.errorCount > analysis.errorAllowed) {
+        smsMessage += smsMessage === "" ? analysis.key : ", " + analysis.key;
+        emailMessage += (emailMessage === "" ? "" : ", ") + emailMessagesByCode[key][0][firstKey];
       }
     }
   }
+  sendNotification(contacts, smsMessage, emailMessage);
 }
+
+module.exports = healthAlarms;
